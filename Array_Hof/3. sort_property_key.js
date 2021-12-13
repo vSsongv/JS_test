4dc8c2be88a965f23e26745beb6c569f6097dd94
vSsongv <songdh21@naver.com>
2021-12-13 14:52:37 +0900

    ✏️ edit

--- Array_Hof/3. sort_property_key.js
+++ Array_Hof/3. sort_property_key.js
@@ -6,7 +6,7 @@ let todos = [
   { id: 1, content: 'Javascript', completed: false },
 ];
 
-const sortBy = (todos, key) => todos.sort((todo1, todo2) => (todo1[key] > todo2[key] ? 1 : -1));
+const sortBy = (todos, key) => todos.sort((todo1, todo2) => (todo1[key] > todo2[key] ? 1 : (todo1[key] < todo2[key] ? -1 : 0)));
 
 todos = sortBy(todos, 'id');
 console.log(todos);
